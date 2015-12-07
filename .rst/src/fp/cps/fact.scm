2413ac2dabaabfbd7735011fe58b7fa8bc75b4ba
her0e1c1 <her0e1c1@gmail.com>
2015-12-07 20:35:01 +0900

    add fact/cps

--- .rst/src/fp/cps/fact.scm
+++ .rst/src/fp/cps/fact.scm
@@ -1,7 +1,7 @@
 
-(define (fact/cps n count)
+; m is the result of "(fact (- n 1))"
+; In "(count XXX)", XXX is the return value the func returns
+(define (fact/cps n cont)
   (if (= n 1)
-      (count 1)
-      ; m is the result of "(fact (- n 1))"
-      ; In "(count XXX)", XXX is the return value the func returns
-      (fact/cps (- n 1) (lambda (m) (count (* n m))))))
+      (cont 1)
+      (fact/cps (- n 1) (lambda (m) (cont (* n m))))))
---
+++ .rst/src/fp/cps/fact.test.scm
@@ -0,0 +1,6 @@
+(add-load-path "." :relative)
+(load "fact.scm")
+(use gauche.test)
+
+(test* "(fact/cps 1)"  1   (fact/cps 1 values))
+(test* "(fact/cps 10)" 120 (fact/cps 5 values))
