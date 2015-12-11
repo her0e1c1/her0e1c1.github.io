ec1d72d4044a561f82c358c8fed0a0250acdc058
her0e1c1 <her0e1c1@gmail.com>
2015-12-11 10:14:09 +0900

    rename

--- .rst/src/c/vector/init.c
+++
@@ -1,9 +0,0 @@
-list_t *init(int data) {
-  list_t* newNode = (list_t*) malloc(sizeof(list_t));
-  if (newNode == NULL) {
-    exit(1);
-  }
-  newNode->data = data;
-  newNode->next = NULL;
-  return newNode;
-}
--- .rst/src/c/vector/search.c
+++
@@ -1,8 +0,0 @@
-list_t *search(list_t *head, int data) {
-  while (head) {
-    if (head->data == data)
-      return head;
-    head = head->next;
-  }
-  return NULL;
-}
