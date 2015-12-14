d8a627b45ca80493813219a34fef36cb052309de
her0e1c1 <her0e1c1@gmail.com>
2015-12-14 20:03:29 +0900

    add string.sh

---
+++ .rst/snippets/ce/string.sh
@@ -0,0 +1,43 @@
+HERE=$(dirname $0)
+cat <<EOS
+
+========
+ String
+========
+
+$(go <<EOG
+ (define (run cmd)
+   (p (sphinx-block (format "$ ce '~a'\n~a" cmd (run-ce cmd)) :code-block "sh")))
+
+ (p (sphinx-section "strcat"))
+ (p "Append strings")
+ (run "char s[]=\"abcde\", *t=\"hijk\" ; strcat(s, t); p(\"%s\", s);")
+ (run "char s[]=\"abcde\", t[]=\"hijk\"; strcat(s, t); p(\"%s\", s);")
+
+ (p "error! if pointer is used, it can't be modified. it is static allocated.")
+ (run "char *s=\"abcde\", *t=\"hijk\" ; strcat(s, t); p(\"%s\", s);")
+ (run "char *s=\"abcde\", t[]=\"hijk\"; strcat(s, t); p(\"%s\", s);")
+
+;; // '\0'に対して追加もできる
+ (run "char s[]=\"\", t[]=\"abcde\"; strcat(s, t); p(\"%s\", s);")
+;; // ce 'char p[]="abcde", q[]="AAA"; memcpy(p,q,3+1); printf("%s", p);'
+;; // ce 'char *p="abcdebce", *q="bc"; printf("%p", strstr(p, q));' 
+
+ (p (sphinx-section "strstr"))
+ (p "if searching an empty string, it is always true.")
+ (run "p(\"(%s)\", strstr(\"\", \"\"));")
+ (run "p(\"%s\", strstr(\"abcde\", \"\"));")
+ (run "p(\"%s\", strstr(\"abcde\", \"ABC\"));")
+
+ (p (sphinx-section "sprintf"))
+ (p "Number to string")
+ (run  "char b[32]; sprintf(b, \"%d\", 12345); p(\"%s\", b);")
+
+ (p "In case of static declaration of string, it can't be modified.")
+ (run "char *c =\"Memory allocated static  string\"; c[1] = 'E'; p(\"NOT REACHED => %s\", c);")
+ (run "char c[]=\"Memory allocated dynamic string\"; c[1] = 'E'; p(\"REACHED => %s\", c);")
+
+EOG
+)
+
+EOS
