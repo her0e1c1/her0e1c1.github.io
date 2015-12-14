9062debe12a06eaaea91f0439f07e5e654d84e44
her0e1c1 <her0e1c1@gmail.com>
2015-12-14 11:17:50 +0900

    rm ; and \n

--- .rst/snippets/ce/1.sh
+++ .rst/snippets/ce/1.sh
@@ -28,17 +28,16 @@ $(go <<EOG
  (p (sphinx-section "memset"))
 
  (p "Fill 0")
- (run "int a[10][10]; memset(a, 0, sizeof(a)); p(\"%d\", a[0][0]);");
+ (run "int a[10][10]; memset(a, 0, sizeof(a)); p(\"%d\", a[0][0]);")
  
  (p "Fill -1")
- (run "int a[10][10]; memset(a, -1, sizeof(a)); p(\"%d\", a[0][0]);");
+ (run "int a[10][10]; memset(a, -1, sizeof(a)); p(\"%d\", a[0][0]);")
 
  (p "Don't work because memset fills numbers but 0 or -1 at each byte")
- (run "int a[10][10]; memset(a, 1, sizeof(a)); p(\"%d\", a[0][0]);");
- (run "int a[10][10]; memset(a, 10, sizeof(a)); p(\"%d\", a[0][0]);");
+ (run "int a[10][10]; memset(a, 1, sizeof(a)); p(\"%d\", a[0][0]);")
+ (run "int a[10][10]; memset(a, 10, sizeof(a)); p(\"%d\", a[0][0]);")
 
 EOG
 )
-
 
 EOS
