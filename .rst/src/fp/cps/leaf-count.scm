ca9a7bcd5b1a1659d44d2ecac7effd4f43c4feea
her0e1c1 <her0e1c1@gmail.com>
2015-12-16 01:54:08 +0900

    rm moved code

--- .rst/src/fp/cps/leaf-count.scm
+++
@@ -1,6 +0,0 @@
-
-(define (leaf-count/cps tree k)
-  (if (pair? tree)
-      (leaf-count/cps (car tree) (lambda (n)
-                        (leaf-count/cps (cdr tree) (lambda (m) (k (+ n m))))))
-      (k 1)))
