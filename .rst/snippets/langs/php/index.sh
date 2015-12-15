76f77b19a7107431913762ad8511cb808b1dbad8
her0e1c1 <her0e1c1@gmail.com>
2015-12-15 19:39:16 +0900

    add php

--- .rst/snippets/langs/php/index.sh
+++ .rst/snippets/langs/php/index.sh
@@ -1,5 +1,4 @@
-HERE=$(dirname $0)
-cat <<EOS
+HERE=$(dirname $0) && cat <<EOS
 =====
  PHP
 =====
@@ -8,25 +7,10 @@ cat <<EOS
 
   curl -O psysh.org/psysh && php psysh
 
-php -r 'echo "hello world";'
+::
 
-<?php
+   php -r 'echo "hello world";'
 
-if (1) {
-    echo "1 is true\n";
-}
-
-$dict = array(5 => 1, 12 => 2);
-echo "$dict[5]\n";
-
-$arr = array(10, 20, 30, 40, 50);
-echo "$arr[0]\n";
-
-
-class A {
-    public $var = 'a default value';
-}
-$a = new A();
-echo "$a->var\n";
+$(go "$HERE/index.scm")
 
 EOS
