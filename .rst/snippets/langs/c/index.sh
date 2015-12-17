106accb3644ad15eac7e24a6d7dae92d7befbdc2
her0e1c1 <her0e1c1@gmail.com>
2015-12-17 13:28:58 +0900

    ignore errror

--- .rst/snippets/langs/c/index.sh
+++ .rst/snippets/langs/c/index.sh
@@ -9,7 +9,7 @@ $(goshe '
  (let ((f (^x
           (let1 it (join-line (list (sphinx-section x :up #t) (sphinx-contents :depth 2)))
            (print (sphinx-include-scm-list (sort (glob #"./s/~|x|/*.scm")) #"~|x|.rst" :header it))))))
- (for-each f (list "array" "string" "grammer" "stdlib" "misc"))
+ (for-each ($ ignore $ f $) (list "array" "string" "grammer" "stdlib" "misc"))
  )
 ')
 
