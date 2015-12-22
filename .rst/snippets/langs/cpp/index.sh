56a6599b19cf753840b181af896f9c58d6c9f9bd
her0e1c1 <her0e1c1@gmail.com>
2015-12-22 20:18:19 +0900

    rename

--- .rst/snippets/langs/cpp/index.sh
+++ .rst/snippets/langs/cpp/index.sh
@@ -4,13 +4,7 @@ cd $(dirname $0) > /dev/null 2>&1 && cat <<EOS
 C++ Language
 ============
 
-$(goshe '
- (let ((f (^x
-          (let1 it (join-line (list (sphinx-section x :up #t) (sphinx-contents :depth 2)))
-           (print (sphinx-include-scm-list (sort (glob #"./~|x|.scm")) #"~|x|.rst" :header it))))))
- (for-each ($ ignore $ f $) (list "vector" "string"))
- )
-')
+$(goshe '(sphinx-import-each-directory "s")')
 
 EOS
 
