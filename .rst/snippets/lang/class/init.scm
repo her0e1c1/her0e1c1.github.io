37f625553856e53642447961c5c7ced15320e633
her0e1c1 <her0e1c1@gmail.com>
2015-12-23 19:44:43 +0900

    add ruby class

--- .rst/snippets/lang/class/init.scm
+++ .rst/snippets/lang/class/init.scm
@@ -1,2 +1,13 @@
 
 (php "class A {public $v = 'val';} $a=new A(); echo \"$a->v\";")
+
+(ruby #!DOC EOS
+class Human
+  def initialize(name="")
+    @name = name
+  end
+  attr_accessor :name
+end
+puts (Human.new "Bob").name
+EOS
+:str #t)
