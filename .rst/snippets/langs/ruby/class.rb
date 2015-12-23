74852014dbb33fdd1f361544035842a39b346b31
her0e1c1 <her0e1c1@gmail.com>
2015-12-23 19:52:51 +0900

    rm ruby sample

--- .rst/snippets/langs/ruby/class.rb
+++
@@ -1,9 +0,0 @@
-# $(goshe '(p (sphinx-run-path-ruby "class.rb"))')
-class Human
-  def initialize(name="")
-    @name = name
-  end
-  attr_accessor :name
-end
-
-puts (Human.new "Bob").name
--- .rst/snippets/langs/ruby/include-ruby.scm
+++
@@ -1,6 +0,0 @@
-(load-from-current-dirctory "include.scm")
-
-(define run oneliner-run+)
-(define (run+ cmd)
-  (let1 cmd+ (quote-single->double cmd)
-        (oneliner-simple-run-print #"ruby -e '~cmd+'")))
