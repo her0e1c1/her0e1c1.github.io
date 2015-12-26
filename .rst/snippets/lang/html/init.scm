6588279298011f6893b6209e69884c35d47619d0
her0e1c1 <her0e1c1@gmail.com>
2015-12-26 10:49:36 +0900

    add js code

--- .rst/snippets/lang/html/init.scm
+++ .rst/snippets/lang/html/init.scm
@@ -1,9 +1,15 @@
-
-;; todo: using .. raw:: html
-;; // Scroll down in 1.2 sec
-;; $("html,body").animate({scrollTop:1000}, 1200);
-
-;; history.back();
-;; history.forward();
-
 ;; .. warning:: formタグはネストできません。
+
+(js "alert('hello')")
+
+(js "history.back()" :msg "<=> forward")
+
+(js "$('html,body').animate({scrollTop:1000}, 1200);"
+    :msg "Scroll to 1000px from the top in 1.2 sec. (arguments are px and time)")
+
+
+(dotimes (100)
+         (print "HOGE\n\n"))
+
+(js "$('html,body').animate({scrollTop:0}, 2000);"
+    :msg "Scroll to 0px in 2 sec")
