c1c8e0a3c0dafb97a3acbfc89c72d59adecada3f
her0e1c1 <her0e1c1@gmail.com>
2015-12-28 01:37:29 +0900

    sh

--- .rst/snippets/lang/io/signal.scm
+++ .rst/snippets/lang/io/signal.scm
@@ -9,3 +9,22 @@ echo P
 wait
 "
     :msg "catch the signal when a child process exit")
+
+(sh "
+set -e
+trap 'echo EXIT' EXIT
+
+echo 1
+
+false && false
+
+echo 2
+
+false && true
+
+echo 3
+
+false
+
+echo 4
+" :str #t :warn "ignore false && false ???")
