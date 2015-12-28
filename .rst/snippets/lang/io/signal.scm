dfd1cc9c953466ca25fea950c5409207c3a2d8cd
her0e1c1 <her0e1c1@gmail.com>
2015-12-28 09:45:37 +0900

    sh

--- .rst/snippets/lang/io/signal.scm
+++ .rst/snippets/lang/io/signal.scm
@@ -1,5 +1,5 @@
 
-(perl "say keys %SIG")
+(perl "say join q/, /, keys %SIG")
 
 
 (sh "
@@ -13,18 +13,37 @@ wait
 (sh "
 set -e
 trap 'echo EXIT' EXIT
-
 echo 1
-
 false && false
-
 echo 2
-
 false && true
-
 echo 3
-
 false
-
 echo 4
 " :str #t :warn "ignore false && false ???")
+
+
+(sh "
+set -e
+1
+echo 1
+" :str #t)
+(sh "
+set -e
+1 && ls HOGE
+echo 1
+" :str #t :warn "ignore an error")
+
+(sh "
+set -e
+trap 'echo EXIT' EXIT
+echo 1
+false || true
+echo 2
+false || false
+echo 3
+" :str #t)
+
+(sh "[ `ls` ] && echo OK" :warn "SYNTAX ERROR!")
+(sh "[ -n \"`ls`\" ] && echo OK" )
+(sh "[ -z \"`ls HOGE`\" ] && echo NO" )
