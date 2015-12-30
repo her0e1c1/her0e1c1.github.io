e6468ff863e55b766385b254158efaa45b9cd6fe
her0e1c1 <her0e1c1@gmail.com>
2015-12-30 10:31:57 +0900

    insert sort

---
+++ .rst/snippets/algo/sort/insert-sort.scm
@@ -0,0 +1,21 @@
+(define DATA1 (generator->list (integers$ 200 -100) 20))
+(define (to-s x) (string-join (map x->string x) ", "))
+
+(p "左端から順番に小さいものを移動")
+
+(c #"
+#include <myc.h>
+void swap(int *x, int *y) { int t=*x; *x=*y; *y=t; }
+void sort(int *a, int len) {
+ for (int i = 0; i < len; i++) {
+  // i番目に次点での最小値を決めたい. (常にa[i]と比較)
+  for (int j = i + 1; j < len; j++)
+   if (a[i] > a[j]) swap(&a[i], &a[j]);
+ }
+}
+int main() {
+ int a[] = {~(to-s DATA1)};
+ sort(a, SIZE(a));
+ PVI(a);
+}
+" :str #t)
--- .rst/snippets/algo/sort/insert_sort/insert_sort.hs
+++
@@ -1,8 +0,0 @@
-
-insertSort :: (a -> a -> Ordering) -> [a] -> [a]
-insertSort p [] = []
-insertSort p (x:xs) = decend x $ insertSort p xs where
-  decend x [] = [x]
-  decend x yys@(y:ys)  -- yysはソート済み
-    | p x y == LT = x:yys
-    | otherwise   = y:decend x ys
