b141e676599b3258fc30de33c5f407fe95e7ec9a
her0e1c1 <her0e1c1@gmail.com>
2016-01-04 23:28:42 +0900

    bubble-sort

--- .rst/snippets/algo/sort/bubble-sort.scm
+++ .rst/snippets/algo/sort/bubble-sort.scm
@@ -1,37 +1,31 @@
-// IMPLEMENT: bubble sort
-#include <stdio.h>
-#include <string.h>
 
-// bubble sortは末尾からsortされる
+(p"
+bubble sortは末尾からsortされる
 
-// 1回目   : 0 <= j <= size - 1
-// 2回目   : 0 <= j <= size - 2
-// size回目: 0 <= j <= size - size = 0
+- 1回目     : 0 <= j <= size - 1
+- 2回目     : 0 <= j <= size - 2
+- size-1回目: 0 <= j <= size - (size-1) = 1
+- size回目  : 0 <= j <= size - size = 0 (meaningless)
 
-void bsort(int *a, int size) {
-  // jの終端を正しく記述できるかがポイント
-  for (int end = size - 1; end >= 0 ; end--) {
-    for (int j = 0; j <= end ; j++) {
-      if (a[j + 1] < a[j]) {
-        int t = a[j];
-        a[j] = a[j+1];
-        a[j+1] = t;
-      }
+")
+
+(c #!Q
+#include <myc.h>
+void swap(int *x, int *y) { int t=*x; *x=*y; *y=t; }
+void sort(int *a, int size) {
+  for (int end = size - 1; end >= 1 ; end--) {
+    for (int j = 0; j < end ; j++) {
+      if (a[j] > a[j + 1])
+       swap(&a[j], &a[j+1]);
     }
   }
 }
-
 int main() {
-  // int A[] = {4, 1, 45, 6, 10, 8, 3};
-  int A[] = {1, 4, 45, 3, 7, 6, 7, 7, 7, 10, 8, 8, 8, 8,
-             1, 10, -1, 3, 20, 5, 9, 33, -1, 4,5,3,6};
-  int arr_size = sizeof(A)/sizeof(A[0]);
-  bsort(A, arr_size);
-  for (int i = 0; i < arr_size; i++)
-    printf("%d, ", A[i]);
-  return 0;
+  int a[] = {1, 4, 45, 3, 7, 6, 7, 7, 7, 10, 8, 8, 8, 8,1, 10, -1, 3, 20, 5, 9, 33, -1, 4,5,3,6};
+  sort(a, SIZE(a));
+  PVI(a);
 }
-
+Q :str #t)
 
 ;; bubbleSort :: Ord a => [a] -> [a]
 ;; bubbleSort xs = case sort xs of
