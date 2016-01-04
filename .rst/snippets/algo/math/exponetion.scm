57839e6a7d60bdb53e2b833f904d99d29a12b845
her0e1c1 <her0e1c1@gmail.com>
2016-01-04 23:29:56 +0900

    exponetion

---
+++ .rst/snippets/algo/math/exponetion.scm
@@ -0,0 +1,28 @@
+
+(math "
+a^n = $begin{cases}
+ 1 & ({n = 0)
+ a^{n/2} * a^{n/2} & ($text{n is even})
+ a^{(n-1)/2} * a^{(n-1)/2} * a & ($text{n is odd})
+$end{cases}
+")
+
+(cpp #!Q
+#include <myutils.h>
+int f(int a, int n) {
+ if (n == 0)
+  return 1;
+ else if (n % 2 == 0) {
+  int x = f(a, n / 2);
+  return x * x;
+ } else {
+  int x = f(a, (n - 1) / 2);
+  return x * x * a;
+ }
+}
+int main() {
+ P(f(2, 10));
+ P(f(3, 2));
+ P(f(4, 3));
+}
+Q :str #t)
