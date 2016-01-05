b720be7abffe618b5f8c5a1b41e00e41f6aff52f
her0e1c1 <her0e1c1@gmail.com>
2016-01-05 09:09:32 +0900

    math

---
+++ .rst/snippets/algo/statictics/index.scm
@@ -0,0 +1,21 @@
+
+(ps "arithmetic mean")
+
+(math "$overline{x} = $frac{$sum_{i=1}^{n} x_i}{n}")
+
+(ps "Cross Validation")
+(p "標本を集めるのが困難な場合などは交差検定を行う.
+これは 標本データの一部を解析して残る部分でテストを行う手法.")
+
+(p"
+Question
+========
+.. toctree::
+   :glob:
+
+   q/*
+
+References
+==========
+- http://ruby.kyoto-wu.ac.jp/~konami/Text/Statistics.pdf
+")
