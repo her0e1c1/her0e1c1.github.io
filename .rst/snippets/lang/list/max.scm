9cf54a214705a64031b41ff4795f8d9a35591797
her0e1c1 <her0e1c1@gmail.com>
2016-01-06 11:54:24 +0900

    min/max

--- .rst/snippets/lang/list/max.scm
+++ .rst/snippets/lang/list/max.scm
@@ -1,2 +1,3 @@
 (emacs (-max '(1 2 3 4 5)))
 (emacs (--max-by (> (-sum it) (-sum other)) '((1 2 3) (4 5 6) (7 8 9))))
+(c "P(\"%d\",MAX(1,MAX(2,3)));")
--- .rst/snippets/lang/list/min.scm
+++ .rst/snippets/lang/list/min.scm
@@ -4,3 +4,6 @@
 
 (emacs (-min '(1 2 3 4 5)))
 (emacs (--min-by (> (-sum it) (-sum other)) '((1 2 3) (4 5 6) (7 8 9))))
+
+(java "P(Math.min(1,-1));")
+(c "P(\"%d\",MIN(1,-1));" :msg "use macro")
