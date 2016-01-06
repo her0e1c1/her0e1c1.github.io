41058529e4ac3fa0624cfabb0f371024ee41a6ce
her0e1c1 <her0e1c1@gmail.com>
2016-01-06 12:10:33 +0900

    mv

--- .rst/snippets/algo/tree/flatten.hs
+++
@@ -1,6 +0,0 @@
-
--- squishの第二引数は結果を保持
-data Tree a = Node a [Tree a] deriving Show
-flatten :: Tree a -> [a]
-flatten t = squish t []
-  where squish (Node x ts) xs = x:foldr squish xs ts
--- .rst/snippets/algo/tree/flatten.scm
+++ .rst/snippets/algo/tree/flatten.scm
@@ -10,3 +10,24 @@
 (print
 (flatten t1)
 )
+
+
+;; -- squishの第二引数は結果を保持
+;; data Tree a = Node a [Tree a] deriving Show
+;; flatten :: Tree a -> [a]
+;; flatten t = squish t []
+;;   where squish (Node x ts) xs = x:foldr squish xs ts
+
+(define (flatten tree)
+  (fold (lambda (x acc)
+          (append acc (if (pair? x)
+                          (flatten x)
+                          (list x))))
+        '()
+        tree))
+
+(define t1 '(a (b (c d) e) (f g) h))
+
+(print
+(flatten t1)
+)
