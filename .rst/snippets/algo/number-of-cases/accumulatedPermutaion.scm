26fc1f3ae10fa4e10c297b6ff81fea6dccde7d5d
her0e1c1 <her0e1c1@gmail.com>
2016-01-08 19:54:17 +0900

    add math

--- .rst/snippets/algo/number-of-cases/accumulatedPermutaion.scm
+++ .rst/snippets/algo/number-of-cases/accumulatedPermutaion.scm
@@ -1,5 +1,11 @@
 (p "異なるn個の中から重複を許さずr個以下を取り出す場合の数(nPr (0 <= ALL r <= n))")
 
+(math "
+P(n, 0) &=& ${ $phi $}
+P(0, r) &=& $phi
+P(n, r) &=& ${ a_n $} $cup P(n-1,r) $cup ${cons(a_n, x) $mid x $in P(n-1, r-1) $}
+")
+
 (ghc #!Q
 p :: [Int] -> Int -> [[Int]]
 p = go where
