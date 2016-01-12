0b84ccb4849afcd16274a2ab9cbe5459a8aac54d
her0e1c1 <her0e1c1@gmail.com>
2016-01-12 15:00:23 +0900

    knapsack

--- .rst/snippets/algo/seq/knapsack.scm
+++ .rst/snippets/algo/seq/knapsack.scm
@@ -1,18 +1,24 @@
--- IMPLEMENT: knapsack
 
-{-
-  問題としては以下
-  n個の詰め物があり、サイズとその価値が決まっている。
-  このとき、ナップザックのサイズのmax_sizeとし、詰め物の価値を最大にする組は何か？
+(p "
+問題としては以下
+n個の詰め物があり、サイズとその価値が決まっている。
+このとき、ナップザックのサイズのmax_sizeとし、詰め物の価値を最大にする組は何か？
+なお、総当たりの場合の場合の数は2^nとなる
+")
 
-  以下の問題に帰着
-  [(a1, b1), (a2, b2),,, (an, bn)] -- [(詰め物のサイズ, その価値)]
-  (sum ai * xi) <= max_size (ただし、xi in {0, 1})
-  max (xi) st sum bi * xi
+(math "
+${ (a_i, b_i) = (詰め物のサイズ, その価値) $}
+$sum_i^n a_i $times x_i <= max_size (ただし、x_i $in {0, 1})
+S.T. $max_{x_i} $sum_i^n b_i $times x_i
+")
 
-  総当たりの場合の場合の数は2^nとなる
--}
+(math "
+f(i, size) = $begin{cases}
+max ${f(i-1, size), f(i-1, size+a_i) + b_i (if size + a_i <= max_size) $}
+$end{cases}
+")
 
+(ghc #!Q
 -- pairs = [(size, price)]
 knapsack :: (Ord a, Num a) => [(a, a)] -> a -> a
 knapsack pairs max_size = go pairs 0 where
@@ -23,15 +29,11 @@ knapsack pairs max_size = go pairs 0 where
         cap1 = go sps size  -- 現在の(s, p)を使わない
         cap2 = if ssize <= max_size then p + go sps ssize else 0
       in maximum [cap1, cap2]
+main = print $ knapsack [(x, 6-x) |x <- [1..5]] 6
+Q :str #t)
 
-main = do
-  print $ knapsack [(x, 6-x) |x <- [1..5]] 6  -- 12
-
-
+(cpp #!Q
 #include <myutils.h>
-
-// ORDER: O(sizes.size() * capacity)  << 2^sizes.size()
-
 int solve(vector<int> sizes, vector<int> prices, int capacity) {
   assert(sizes.size() == prices.size());
 
@@ -60,5 +62,6 @@ int solve(vector<int> sizes, vector<int> prices, int capacity) {
 int main() {
   P(solve({1,2,3,4,5},
           {5,4,3,2,1},
-          6));  // 12
+          6));
 }
+Q :str #t)
