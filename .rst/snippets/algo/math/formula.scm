12f01efd97386990aa2386cb2d70a71da34c01b5
her0e1c1 <her0e1c1@gmail.com>
2016-01-14 18:03:46 +0900

    いろいろ

--- .rst/snippets/algo/math/formula.scm
+++ .rst/snippets/algo/math/formula.scm
@@ -1,14 +1,3 @@
-(define DATA1 (generator->list (integers$ 20 -10) 10))
-
-(ps "配列の３つの積の最大値は?")
-(math "$max_{i,j,k $in N} a[i] $times a[j] $times a[k]")
-(eval-null `(gosh
- #"(print
-    (let1 l (pa$ at (sort '~DATA1))
-       (max (* (l -1) (l -2) (l -3))
-            (* (l 0) (l 1) (l -1)))))"
- :str #t))
-
 (ps "特定の区間にある整数")
 (math "$# ${i| A $leqq i $leqq B $}")
 (gosh (let1 f (^(A B) (if (>= A B) 0 (+ (- B A) 1))) (f 3 9)))
