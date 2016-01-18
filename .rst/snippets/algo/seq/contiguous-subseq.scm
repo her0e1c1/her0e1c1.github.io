857de94438d279227de72677259b1aa7e810b95b
her0e1c1 <her0e1c1@gmail.com>
2016-01-18 09:53:38 +0900

    fix equation

--- .rst/snippets/algo/seq/contiguous-subseq.scm
+++ .rst/snippets/algo/seq/contiguous-subseq.scm
@@ -2,25 +2,43 @@
 (pw "ある数列の連続した部分数列の和の最大値")
 
 (math "
-f(1) = ${ a_1 $}
-f(n) = $max ${ f(n-1) + a_n, a_n $}
+f(i) = ($mbox{iにおける最大}, $mbox{iにおける総和}) = (m_i, s_i)
+s_0 = m_0 = 0
+s_i = $max ${ 0, s_{i-1} + a_i $}
+m_i = $max ${ m_{i-1}, s_i $}
 ")
 
 (ghc #!Q
-f = go where
- go [x] = x
- go (x:xs) = max x ((go xs) + x)
-main = print $ f [1,-2,1,3,2,-5,1,-4,-3,1,-2]
-Q:str #t)
+f = go 0 0 where
+ go s m [] = m
+ go s m (x:xs) = go y n xs where
+  y = max (s + x) 0
+  n = max m y
+main = do
+ print $ f [0,4,-6,2,3]
+ print $ f [1,-2,1,3,2,-5,1,-4,-3,1,-2]
+Q:str #t :msg "大きい負の数が来たら、足さずに0から再度開始")
 
-(p "result of a list")
+(ghc #!Q
+s :: [Int] -> Int
+s [] = 0
+s (x:xs) = max 0 (x + s xs)
+m :: [Int] -> Int
+m [] = 0
+m (x:xs) = max (m xs) (s $ x:xs)
+main = do
+ print $ m [0,4,-6,2,3]
+ print $ m [1,-2,1,3,2,-5,1,-4,-3,1,-2]
+Q:str #t)
 
 (ghc #!Q
 import Data.List (maximumBy)
-f :: [Int] -> [Int]
-f = go where
- go [x] = [x]
- go (x:xs) = maximumBy cmp [[x], (x:go xs)]
- cmp a b = compare (sum a) (sum b)
-main = print $ f [1,-2,1,3,2,-5,1,-4,-3,1,-2]
-Q:str #t)
+c a b = compare (sum a) (sum b)
+s [] = []
+s (x:xs) = maximumBy c [[], (x:(s xs))]
+m [] = []
+m (x:xs) = maximumBy c [(m xs), (s(x:xs))]
+main = do
+ print $ m [0,4,-6,2,3]
+ print $ m [1,-2,1,3,2,-5,1,-4,-3,1,-2]
+Q:str #t :msg "result of a list")
