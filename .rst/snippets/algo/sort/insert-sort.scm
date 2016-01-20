dbb4e5aaf3d7a73b4888a3894b08915a32abe3a2
her0e1c1 <her0e1c1@gmail.com>
2016-01-20 11:56:40 +0900

    add math for insert sort

--- .rst/snippets/algo/sort/insert-sort.scm
+++ .rst/snippets/algo/sort/insert-sort.scm
@@ -7,6 +7,15 @@
 (p "挿入については、適した位置が見つかれば、その時点でi番目までソート済みになる。
 そのため、ソート済み配列をソートする場合の計算量は、O(n)となる。")
 
+(math #!Q
+S_n := ${vector(a_1, a_2, ... a_n)}
+f(S_i) = insert f(S_{i-1}) a_i
+insert(S_0, a_{i+1}) = ${ vector(a_{i+1}) $}
+insert(S_i, a_{i+1}) = $begin{cases} append(a_{i+1}, S_i) & (a_i < a_{i+1})
+append(a_i, insert(S_{i-1}, a_{i+1}))
+$end{cases}
+Q:str #t)
+
 (c #"
 #include <myc.h>
 static inline void swap(int *x, int *y) { int t=*x; *x=*y; *y=t; }
@@ -25,3 +34,15 @@ int main() {
  PVI(a);
 }
 " :str #t)
+
+(ghc #!Q
+insert [] y = [y]
+insert (x:xs) y
+ | x < y = x:insert xs y
+ | otherwise = y:x:xs
+sort [] = []
+sort (x:xs) = insert (sort xs) x
+main = print $ sort [3,1,2,5,4]
+Q
+:str #t
+:msg "ソート済み配列に対して、適切な位置にyを挿入(本来は後ろからswap)")
