fb38304828b55a5c9622dbdf4416bcf0a4aeb9ed
her0e1c1 <her0e1c1@gmail.com>
2016-01-20 11:06:34 +0900

    bubble sort

--- .rst/snippets/algo/sort/bubble-sort.scm
+++ .rst/snippets/algo/sort/bubble-sort.scm
@@ -1,12 +1,20 @@
 
-(p"
-bubble sortは末尾からsortされる
+(p"bubble sortは末尾からsortされる ::
 
-- 1回目     : 0 <= j <= size - 1
-- 2回目     : 0 <= j <= size - 2
-- size-1回目: 0 <= j <= size - (size-1) = 1
-- size回目  : 0 <= j <= size - size = 0 (meaningless)
+   1回目     : 0 <= j <= size - 1
+   2回目     : 0 <= j <= size - 2
+   size-1回目: 0 <= j <= size - (size-1) = 1
+   size回目  : 0 <= j <= size - size = 0 (meaningless)
+")
 
+(math "
+数列a_N = (a_1 a_2 ... a_N) = $hat{a} S.T.
+g(N) &=& $hat{a}
+g(i-1) &=& f(i, i)
+f(0, end) &=& g(end)
+f(j+1, end) &=& $begin{cases} swap(f(j, end), j, j+1) & (IF a_j > a_{j+1})
+f(j, end) & (else $mbox{j >= endの場合、f(j, end) = f(j+n, end)なので、プログラム上はループ終了})
+$end{cases}
 ")
 
 (c #!Q
@@ -27,22 +35,13 @@ int main() {
 }
 Q :str #t)
 
-;; bubbleSort :: Ord a => [a] -> [a]
-;; bubbleSort xs = case sort xs of
-;;   (x:xs) -> x:bubbleSort xs
-;;   x      -> x
-;;   where
-;;   sort (x:y:zs)
-;;     | x < y     = y:sort (x:zs)
-;;     | otherwise = x:sort (y:zs)   sort x = x
-
-;;  (define (descend l a)
-;;    (if (or (null? l) (< a (car l)))
-;;        (cons a l)
-;;        (cons (car l) (descend (cdr l) a))))
-
-;; ; 一つ前のソート済みの配列に、合致する位置に挿入
-;; ; f(n) = (descend f(n-1) a)
-;; (define (bubble-sort l)
-;;   (if (null? l) l
-;;       (descend (bubble-sort (cdr l)) (car l))))
+(ghc #!Q
+f [] = []
+f xs = f (take (length ys - 1) ys) ++ [last ys] where
+ ys = g xs
+g [x] = [x]
+g (x:y:zs)
+ | x < y     = x:g (y:zs)
+ | otherwise = y:g (x:zs)
+main = print $ f [3,1,2,5,4]
+Q :str #t)
