250c16361ba6f5c5a50fb7dce51b12d220a574cb
her0e1c1 <her0e1c1@gmail.com>
2016-01-22 19:50:45 +0900

    LCS

--- .rst/snippets/algo/seq/longest-common-sequence.scm
+++ .rst/snippets/algo/seq/longest-common-sequence.scm
@@ -1,5 +1,5 @@
 
-(p "２数列の最長共通部分数列")
+(p "２数列の最長共通部分数列(連続する必要なし)")
 
 (math "
 f(x, y) = $begin{cases} 0 (x = 0 or y = 0)
@@ -9,3 +9,25 @@ $end{cases}
 ")
 
 (p "１変数のみ動かし、他の変数を固定して考えるのだと思うけど、まだよくわかってない")
+
+(cpp #!Q
+#include "myutils.h"
+string f(string a, string b) {
+  function <string(int, int)> loop = [&] (int i, int j) {
+    if (i == a.size() || j == b.size())
+      return string();
+    else if (a[i] == b[j])
+      return a[i] + loop(i+1, j+1);
+    string s1 = loop(i, j+1);
+    string s2 = loop(i+1, j);
+    return (s1.size() > s2.size()) ? s1 : s2;
+  };
+  return loop(0, 0);
+}
+int main() {
+  P(f("abcdefg", "ace"));
+  P(f("abcdefg", "kakcke"));
+  P(f("abcdefg", "kakdkbkekcdf"));
+  P(f("azbzczdzezfzg", "kakdkbkekcdf"));
+}
+Q :str #t)
