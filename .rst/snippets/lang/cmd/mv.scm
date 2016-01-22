43b4df8f99cd422f35c22fcfea54385d2c4d65d8
her0e1c1 <her0e1c1@gmail.com>
2016-01-22 09:52:16 +0900

    mv

--- .rst/snippets/lang/cmd/mv.scm
+++ .rst/snippets/lang/cmd/mv.scm
@@ -1,3 +1,7 @@
 
-(run "find ./ -print0 | perl -MFile::Copy -MFile::Basename -l0ne 'move($_, \"/path/to/\" . basename($_)'"
-     :msg "ファイルシステムをまたいでmove")
+; (run "find ./ -print0 | perl -MFile::Copy -MFile::Basename -l0ne 'move($_, \"/path/to/\" . basename($_)'" :msg "ファイルシステムをまたいでmove")
+
+(gosh (let* ((s "a.txt") (d "b.txt"))
+        (touch-file s)
+        (move-file s d)
+        (remove-file d)))
