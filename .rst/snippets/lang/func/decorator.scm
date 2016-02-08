930e2e5309c685c6d7d5df75db72e5a9031f668a
her0e1c1 <her0e1c1@gmail.com>
2016-02-09 00:51:39 +0900

    memorize

--- .rst/snippets/lang/func/decorator.scm
+++ .rst/snippets/lang/func/decorator.scm
@@ -15,3 +15,22 @@ print(add10(20))
 add10 = partial(add, x=10)
 print(add10(y=20))
 " :str #t)
+
+(ps "memo")
+
+(py "
+def memo(f):
+ cache = {}
+ def wrap(*args):
+  if args in cache:
+   return cache[args]
+  cache[args] = f(*args)
+  return cache[args]
+ return wrap
+@memo
+def fib(n):
+ if n == 0 or n == 1:
+  return n
+ return fib(n-1) + fib(n-2)
+print(fib(35))
+" :str #t)
