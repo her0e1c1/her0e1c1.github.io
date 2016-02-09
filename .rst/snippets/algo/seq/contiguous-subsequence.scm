5ecb970aea3f594b00a2bac0e4841273170c83a0
her0e1c1 <her0e1c1@gmail.com>
2016-02-09 10:10:20 +0900

    contiguous-subsequence

--- .rst/snippets/algo/seq/contiguous-subsequence.scm
+++ .rst/snippets/algo/seq/contiguous-subsequence.scm
@@ -2,24 +2,24 @@
 (p "ある数列の連続した部分数列の和の最大値")
 
 (math "
-f(i) = ($mbox{iにおける最大}, $mbox{iにおける総和}) = (m_i, s_i)
-s_1 = a_1
-m_1 = a_1
-s_i = $max ${ a_i, s_{i-1} + a_i $}
-m_i = $max ${ m_{i-1}, s_i $}
+f(i) &=& ($mbox{iにおける最大}, $mbox{iにおける総和}) = (m_i, s_i)
+s_1 &=& a_1
+m_1 &=& a_1
+s_i &=& $max ${ a_i, s_{i-1} + a_i $}
+m_i &=& $max ${ m_{i-1}, s_i $}
 ")
 
 (ghc #!Q
 f (x:xs) = go x x xs where
  go s m [] = m
- go s m (x:xs) = go y n xs where
-  y = max (s + x) x
-  n = max m y
+ go s m (x:xs) = go si mi xs where
+  si = max (s + x) x
+  mi = max m si
 main = do
  print $ f [-10]
  print $ f [0,4,-6,2,3]
  print $ f [1,-2,1,3,2,-5,1,-4,-3,1,-2]
-Q:str #t :msg "大きい負の数が来たら、足さずに0から再度開始")
+Q:str #t :msg "今までの総和よりも大きい数ならば、それを優先")
 
 (ghc #!Q
 s :: [Int] -> Int
@@ -46,3 +46,20 @@ main = do
  print $ m [0,4,-6,2,3]
  print $ m [1,-2,1,3,2,-5,1,-4,-3,1,-2]
 Q:str #t :msg "result of a list")
+
+(cpp #!Q
+#include <myutils.h>
+int f(vector<int> a) {
+ if (a.empty()) return 0;
+ int m, s;
+ m = s = a[0];
+ for (int i = 1; i < a.size(); i++) {
+  s = max(s + a[i], a[i]);
+  m = max(m, s);
+ }
+ return m;
+}
+int main() {
+ P(f({1,-2,1,3,2,-5,1,-4,-3,1,-2}));
+}
+Q :str #t)
