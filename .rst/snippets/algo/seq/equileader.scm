970c2560a3e5fdc5210b1ae518a4a98ab8b4e2f7
her0e1c1 <her0e1c1@gmail.com>
2016-02-09 11:33:53 +0900

    make code readable

--- .rst/snippets/algo/seq/equileader.scm
+++ .rst/snippets/algo/seq/equileader.scm
@@ -1,26 +1,29 @@
 
 (math "
-${a_n$} $mbox{の半分超過の要素x} S.T. 2集合 ${a_1, a_2 ... a_k$}, ${a_{k+1}, a_{k+2} ... a_n $}
-$mbox{が、それぞれxが超過要素となる場合の数}")
+${a_n$} $mbox{の半分超過の要素x} S.T.
+2集合 ${a_1, a_2 ... a_i$}, ${a_{i+1}, a_{i+2} ... a_n $} $mbox{が、それぞれxが超過要素となる場合の数}")
 
 (math "
-f(i, n, s) = f(i+1, N, S)
+f(i, n, s) &=& f(i+1, N, S)
 where
-num = $mbox{${a_n$}におけるxの要素数}
-N = n + 1 (if a_i = x)
-S = s + 1 (if 2n > i + 1, 2(num - n) > length(a_n) - (i + 1))
+num &=& ${a_n$}におけるxの要素数
+N   &=& n + 1 (IF a_i = x)
+S   &=& s + 1 (IF 2n $gl i + 1 and 2(num - n) $gl length(a_n) - (i + 1))
 ")
 
 (py #!Q
-def f(A, d):
- num = len([a for a in A if a == d])
- s = 0
- n = 0
+def f(A, x):
+ num = len([a for a in A if a == x])
+ # nは、i番目までにおけるxの要素数
+ s = n = 0
  for i in range(len(A)):
-  if A[i] == d:
+  if A[i] == x:
    n += 1
+  # 区間[0, i]にて、    xが半分超過すること
+  # 区間[i+1, n-1]にて、xが半分超過すること
   if 2 * n > i + 1 and 2 * (num - n) > len(A) - (i + 1):
    s += 1
  return s
-print(f([4, 3, 4, 4, 4, 2], 4))
+print(f([4, 3, 4, 4, 4, 2], 4))  # ([4], [3,4,4,4,2]), ([4,3,4], [4,4,2])
+print(f([2, 4, 4, 4, 4, 4], 4))
 Q:str #t)
