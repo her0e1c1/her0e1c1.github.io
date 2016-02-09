9f2c3c0b28dbed697c19964e9eeb3cf683ee9c49
her0e1c1 <her0e1c1@gmail.com>
2016-02-09 22:16:41 +0900

    horner

--- .rst/snippets/algo/seq/horner.scm
+++ .rst/snippets/algo/seq/horner.scm
@@ -2,23 +2,28 @@
 (math "y = $sum_{i=0}^{n} a_i x^i")
 
 (math "
-S_0 & = & a_n
-S_1 & = & a_n x + a_{n-1} & = & S_0 * x + a_{n-1}
-S_2 & = & a_n x^2 + a_{n-1} x + a_{n-2} & = & S_1 * x + a_{n-2}
-...
-S_i & = & S_{i-1} * x + a_{n-i}
+S_n &=& a_0 x^n + a_1 x^{n-1} + ... + a_{n-1} x + a_n
+    &=& $sum_{i=0}^{n} a_i x^{n-i}
+    &=& $sum_{i=0}^{n-1} a_i x^{n-i} + a_n
+    &=& x $sum_{i=0}^{n-1} a_i x^{(n-1)-i} + a_n
+    &=& x S_{n-1} + a_n
 ")
 
 (c #!Q
 #include <myc.h>
 int S(int *a, int n, int x) {
- int y = a[n - 1];
- for (int i = n - 2; i >= 0; i--)
-  y = y * x + a[i];
- return y;
+ assert(n > 0);
+ int s = a[0];
+ for (int i = 1; i < n; i++)
+  s = s * x + a[i];
+ return s;
 }
 int main() {
- int a[] = {3,2,1};
+ int a[] = {3,2,1};  // 3x^2 + 2x + 1
  P("%d", S(a, SIZE(a), 2));
 }
 Q :str #t)
+
+(ps "one byte offset")
+(p "last - first = n - 1")
+(math "$sum_{i=1}^{n} a_i = $sum_{i=0}^{n-1} a_{i+1} = $sum_{i=2}^{n+1} a_{i-1}")
