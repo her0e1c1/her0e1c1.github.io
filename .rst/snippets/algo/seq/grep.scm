54c2d09ab7c1b2404efaa79d74c2caf63119600f
her0e1c1 <her0e1c1@gmail.com>
2016-02-09 12:51:05 +0900

    fix a bug

--- .rst/snippets/algo/seq/grep.scm
+++ .rst/snippets/algo/seq/grep.scm
@@ -2,11 +2,10 @@
 (ps "if the pattern matches line, then print the line and the N previous and N next lines")
 
 (math "
-f(i, n, q) = $begin{cases}
-f(i+1, N, ${$}) & (match(a_i, pattern))
+f(i, n, q) = $begin{cases} f(i+1, N, ${$}) & (match(a_i, pattern))
 f(i+1, n-1, ${$})  & (n > 0)
-f(i+1, 0, insert(a_1, q)) & (else)
-$mbox{ただし、qは最大NまでのFIFOデータ構造}
+f(i+1, 0, insert(a_i, q)) & (else)
+$mbox{ただし、qは最大Nまでのfifoデータ構造}
 $end{cases}
 ")
 
@@ -22,10 +21,12 @@ def f(lines, pattern, N):
    n = N
   elif n > 0:
    print(line)
-   n =- 1
+   n -= 1
   else:
    queue.append(line)
-  if len(queue) > N:
-   queue.pop(0)
-f("1 p 3 2 1 4 3 3 4".split(), "3", 2)
+   if len(queue) > N:
+    queue.pop(0)
+f("1 p 3 2 1 4 3 3 4".split(), "3", 1); print("")
+f("1 p 3 2 1 4 3 3 4".split(), "3", 2); print("")
+f("1 p 3 2 1 4 3 3 4".split(), "2", 2); print("")
 Q :str #t)
