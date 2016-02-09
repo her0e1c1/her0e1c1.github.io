609bcd66b4074ea2559189eb9a20d534fd0645e4
her0e1c1 <her0e1c1@gmail.com>
2016-02-09 10:21:37 +0900

    dominator

--- .rst/snippets/algo/seq/dominator.scm
+++ .rst/snippets/algo/seq/dominator.scm
@@ -2,9 +2,8 @@
 (ps "ある数列における、半数超過を占める要素を求める")
 
 (math "
-f(i,stack_size,n) = $begin{cases}
-f(i+1,stack_size+1, n) & (a_i = n)
-f(i+1,stack_size-1, n) & (a_i $ne n, stack_size > 0)
+f(i,stackSize,n) = $begin{cases} f(i+1,stackSize+1, n) & (a_i = n)
+f(i+1,stackSize-1, n) & (a_i $ne n, stackSize > 0)
 f(i+1, 1, a_i) & (else)
 $end{cases}
 ")
@@ -12,7 +11,6 @@ $end{cases}
 (py #!Q
 def f(a):
  def g(i, size, n):
-  print(i, size, n)
   if i == len(a):
    return n
   elif a[i] == n:
@@ -20,11 +18,14 @@ def f(a):
   elif size > 0:
    return g(i+1, size-1, n)
   else:
+   assert size == 0
    return g(i+1, 1, a[i])
- return g(0, 0, None)
- # return g(0, 1, a[0])  # こっちだと半分以上になる?
-print(f([2,1,2,3,4,2,3,4,2,3,2]))
+ d = g(0, 0, None)
+ if 2 * len([x for x in a if x == d]) > len(a):
+  return d
 print(f([1,1,1,2,1,2,3]))
+print(f([1,1,1,1,2,2,2]))
 print(f([1,1,1,2,2,2]))
-print(f([1,1,2,2,3]))  # 候補がない場合のバリデーションも別途必要そう
+print(f([1,1,2,2,3]))
+print(f([2,1,2,3,4,2,3,4,2,3,2,1,1]))
 Q:str #t)
