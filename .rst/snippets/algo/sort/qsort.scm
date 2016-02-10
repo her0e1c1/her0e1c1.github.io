01eabbc69205e15099a7f47dd0a6257caaf415ca
her0e1c1 <her0e1c1@gmail.com>
2016-02-10 10:56:33 +0900

    add partition

--- .rst/snippets/algo/sort/qsort.scm
+++ .rst/snippets/algo/sort/qsort.scm
@@ -194,3 +194,19 @@ int main() {
         (append (quick-sort small) (list pivot) (quick-sort large)))))
 (print  (quick-sort '~(to-i test1)))
 " :str #t))
+
+
+;; int partition(int arr[], int l, int r)
+;; {
+;;  int x = arr[r], i = l;
+;;      for (int j = l; j <= r - 1; j++)
+;;               {
+;;                if (arr[j] <= x)
+;;                   {
+;;                    swap(&arr[i], &arr[j]);
+;;                        i++;
+;;                        }
+;;                   }
+;;               swap(&arr[i], &arr[r]);
+;;               return i;
+;;               }
