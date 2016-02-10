ee951453bbfb4cd5ebd9c5603c2f9d5a6a0c9a8e
her0e1c1 <her0e1c1@gmail.com>
2016-02-10 14:25:57 +0900

    scanf

--- .rst/snippets/lang/io/stdin.scm
+++ .rst/snippets/lang/io/stdin.scm
@@ -8,3 +8,6 @@
 (sh "echo 'echo $1, $2'| sh -s - 100 1")
 
 (zsh "cat <<< 'STRING'")
+
+(run #!Q echo "test hoge"| ce 'char a[10], b[10]; scanf("%s %s", a, b); p(a); p(b);' Q
+ :warn "buffer overrunの危険性はあるが、簡単な入出力方法")
