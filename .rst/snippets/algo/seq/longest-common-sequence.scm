201019f1fac9c5fc66af7c72b4aae25bfb7d4b27
her0e1c1 <her0e1c1@gmail.com>
2016-02-11 09:15:19 +0900

    LCS

--- .rst/snippets/algo/seq/longest-common-sequence.scm
+++ .rst/snippets/algo/seq/longest-common-sequence.scm
@@ -2,14 +2,15 @@
 (p "２数列の最長共通部分数列(連続する必要なし)")
 
 (math "
-f(x, y) = $begin{cases} 0 (x = 0 or y = 0)
-f(x-1, y-1) (x = y)
-max{f(x-1,y), f(x, y-1)}
+f(x, y) = $begin{cases} 0 & (x = 0 or y = 0)
+f(x-1, y-1) & (x = y)
+$max ${f(x-1,y), f(x, y-1) $} & (else)
 $end{cases}
 ")
 
-(p "f(x-1, y-1)は、f(x-1,y)とf(x,y-1)の次の遷移に含まれるから考慮しなくてよい")
-(p "１変数のみ動かし、他の変数を固定して考えるのだと思うけど、まだよくわかってない")
+(p "f(x-1, y-1)は、f(x-1,y)とf(x,y-1)の次の遷移に含まれるから考慮しなくてよい.")
+(p "１変数のみ動かし、他の変数を固定して考えるのだと思うけど、まだよくわかってない.
+ただ、単純に(x,y)の手前は(x-1,y)か(x,y-1)ということで良さそう.")
 
 (ghc #!Q
 import Data.List (maximumBy)
@@ -19,14 +20,13 @@ f = g where
   | x == y    = x : g xs ys
   | otherwise = maximumBy c [(g xss ys), (g xs yss)]
  g _ _ = ""
-p = print <$> f
 main = do
  print $ f "abcdefg" "bde"
  print $ f "abcdefg" "ace"
  print $ f "abcdefg" "kakcke"
- print $ f "abcdefg" "kakdkbkekcdf"
+ print $ f "abcdefg"       "kakdkbkekcdf"
  print $ f "azbzczdzezfzg" "kakdkbkekcdf"
-Q:str #t)
+Q :str #t)
 
 (cpp #!Q
 #include "myutils.h"
