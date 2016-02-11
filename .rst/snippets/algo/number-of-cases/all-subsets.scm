5ff434723fa3cd43498f6949b9ac5c0be4ca887b
her0e1c1 <her0e1c1@gmail.com>
2016-02-11 16:52:44 +0900

    default value

--- .rst/snippets/algo/number-of-cases/all-subsets.scm
+++ .rst/snippets/algo/number-of-cases/all-subsets.scm
@@ -1,27 +1,24 @@
 
 (p "べき乗
 Xを集合とする。 Xの部分集合全体を2^xと表す
+異なるn個の中で構成される全ての組み合わせ")
 
-異なるn個の中で構成される全ての組み合わせ
+(math "
+f(0) &=& {$phi}
+f(n) &=& ${ {a_n} $cup x $mid x $in f(n-1) $} $cup f(n-1)
 ")
 
 (math "
-f(${ a_0 $}) = {$phi}
-f(${ a_n $}) = ${ {a_n} $cup x $mid x $in g $} $cup g
-g = f(${ a_{n-1} $})
+X      &=& ${1,2,3$} のとき
+2^X    &=& ${ $phi,${1$},${2$},${3$},${1,2$},${2,3$},${3,1$},${1,2,3$}$ $}
+$# 2^X &=& 2^3 = {}_3 C _0 + {}_3 C _1 + {}_3 C _2 + {}_3 C _3 = 8
+(つまり、 $sum_{r=0}^n {}_n C _r = 2^n)
 ")
 
-(math "
-X = ${1,2,3$} のとき
-2^X = ${$phi,{1},{2},{3},{1,2},{2,3},{3,1},{1,2,3}$}
-$# 2^X = 2^3 = {}_3 C _0 + {}_3 C _1 + {}_3 C _2 + {}_3 C _3 = 8
-(つまり、 $sum_r^n {}_n C _r = 2^n)
-")
-
-(p "一つ前の結果に、新しく要素を加えるか、加えないかで分けて考える
+(p "一つ前の結果に、新しく要素を加えるか、加えないかで分けて考える.
 f(x:xs)は、[[INT]]なので、xを使う場合とそうでない場合に分ける")
 
-(ghc #!Q
+(ghc "
 comb :: (Num a) => [a] -> [[a]]
 comb = go where
   go []    = [[]]
@@ -29,32 +26,4 @@ comb = go where
 main = do
   print $ comb [1..3]
   print $ comb [1..5]
-Q :str #t)
-
-(cpp #!Q
-#include "myutils.h"
-vector<vector<int>> comb(vector<int>& set, int index) {
-  if (set.size() == index) {
-    return vector<vector<int>>(1, vector<int>());
-  }
-  vector<vector<int>> r;
-  auto q = comb(set, index + 1);
-  for (auto x: q)
-    r.push_back(x);
-  int a = set[index];
-  for (auto x: q) {
-    x.push_back(a);
-    r.push_back(x);
-  }
-  return r;
-}
-int main() {
-  vector<int> x = {1,2,3};
-  vector<vector<int>> xs = comb(x, 0);
-  REP(i, xs.size()){
-    REP(j, xs[i].size())
-      cout << xs[i][j] << ", ";
-    cout << endl;
-  }
-}
-Q :str #t)
+")
