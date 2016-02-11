02c0f2dee2361b3c91adeaf9a26db171ba443527
her0e1c1 <her0e1c1@gmail.com>
2016-02-12 00:14:04 +0900

    set

--- .rst/snippets/algo/set/intersection.scm
+++ .rst/snippets/algo/set/intersection.scm
@@ -2,9 +2,9 @@
 (math "A $cap B")
 
 (py "
-# set(A) & set(B)
 def f(A, B):
  return [x for x in A if x in B]
 print(f([1,2,3,4,5], [1,3,5,7,9]))
 " :str #t)
 
+(py "P(set([1,2,3,4,5]) & set([1,3,5,7,9]))")
