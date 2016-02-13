22e4a8159b41bbeebca8d1f33e290a23bc74849a
her0e1c1 <her0e1c1@gmail.com>
2016-02-13 21:45:04 +0900

    分割数

--- .rst/snippets/algo/liner-algebra/index.scm
+++ .rst/snippets/algo/liner-algebra/index.scm
@@ -24,13 +24,14 @@
 (math "(AB)C = A(BC)" :msg "結合法則")
 
 (ps "転置行列")
-(math "{}^t A = a_{ji}")
+(math "{}^t A = a_{ji} = tr A")
 (math "{}^t (AB) = {}^t B {}^A")
 
 (ps "エルミート行列")
 
 (ps "正則行列")
 (math "XA=AX=E" :msg "n次正方行列Aに対してXが存在すれば、Aは正方行列でXはAの逆行列")
+(math "Aが正則 $Leftrightarrow |A| $ne 0")
 (math "" :msg "逆行列は一意に決まる")
 (math "(AB)^{-1} = B^{-1} A^{-1}")
 
@@ -51,3 +52,11 @@
 (math "|{}_t A| = |A|")
 (math "det (a_1, ..., a_i, ..., a_j, ... a_n) = det (a_1, ... a_i + k a_j, ..., a_j, ... a_n)"
       :msg "多重線形性+交代性(ある列に他の列の定数倍を加えてもよい)")
+(math "det AB = det A det B")
+
+(ps "第1列に関する|A|の余因子展開")
+(math "det A = $sum_{i=1}^n (-1)^{i-1} a_{i1} |i行目以外| = $sum_{i=1} a_{i1} $tilde{a_{i1}}")
+(math "$tilde{a_{ij}} = (-1)^{i+j} $Delta_{ij}" :msg "行列Aからi行j列を除いた行列を第(i,j)小行列D_{ij}と表す.(i,j)余因子")
+
+(ps "逆行列")
+(math "A^{-1} = $frac{$tilde{A}}{|A|}")
