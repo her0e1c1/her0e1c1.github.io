2a2db28210efe5853c8a35959876427147c9917e
her0e1c1 <her0e1c1@gmail.com>
2016-03-10 22:04:15 +0900

    up

--- .rst/snippets/algo/liner-algebra/eigenvalue.scm
+++ .rst/snippets/algo/liner-algebra/eigenvalue.scm
@@ -8,8 +8,9 @@
 
 
 (ps "対角化")
-
+(math "Aが対角化可能 $Leftrightarrow n個の1次独立な固有ベクトルをもつ")
+(math "n個の異なる固有値をもつ $rightarrow Aは対角化可能")
 
 (ps "ケイリー・ハミルトンの定理")
 
-(math "対角化可能 $Leftrightarrow AB=BA")
+(math "行列AとB対角化可能 $Leftrightarrow AB=BA")
