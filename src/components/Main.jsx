ec0eb415a11f051d258c959f25e31c3c4589f118
her0e1c1 <her0e1c1@gmail.com>
2017-02-27 20:42:47 +0800

    refactor

---
+++ src/components/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Input from './Input/index.jsx'
+import Message from './Message.jsx'
+import Topic from './Topic.jsx'
+import Header from './Header.jsx'
+
+import {Socket} from './../phoenix.js'
+const t = require('assets/lang/ja.json')
+
+const WEBSOCKET_URL = "ws://128.199.158.226:13309/socket"
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    const socket = new Socket(WEBSOCKET_URL)
+    console.log(socket)
+    socket.connect()
+    socket.onError( () => console.log("there was an error with the connection!") )
+    socket.onClose( () => console.log("the connection dropped") )
+    this.state = {socket}
+  }
+  render() {
+    const {socket} = this.state
+    return (
+    <div>
+      <Header />
+    {t.hello}
+      
+      {/* <footer> &copy; Hiroyuki Ishii 2017. All Rights Reserved </footer> */}
+    </div>
+  )}
+}
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Main from './components/Main.jsx'
+import Analytics from './components/Analytics.jsx'
 
-import Topic from './components/Topic.jsx'
-import Analytics from './components/Analytics/index.jsx'
-import Input from './components/Input/index.jsx'
-import Message from './components/Message.jsx'
-import {Socket} from './phoenix.js'
-
-const t = require('assets/lang/ja.json')
-
-const WEBSOCKET_URL = "ws://128.199.158.226:13309/socket"
 const main = document.getElementById('main')
+ReactDOM.render(<Main />, main)
 
-let socket = new Socket(WEBSOCKET_URL)
-console.log(socket)
-socket.connect()
-socket.onError( () => console.log("there was an error with the connection!") )
-socket.onClose( () => console.log("the connection dropped") )
-
-ReactDOM.render(
-    <div>
-    {t.hello}
-    <Topic socket={socket} />
-    <Topic socket={socket} topic="chat:bot" event="bot" />
-    <Topic socket={socket} topic="chat:client" event="from_slack" />
-    </div>, main)
-
-const head = document.createElement('div')
-// const head = document.getElementsByTagName("head")
+const head = document.getElementsByTagName("head")[0]
 ReactDOM.render(<Analytics/>, head)
-document.head.appendChild(head)
-
-const headers = {
-  'Content-Type': 'application/json',
-}
-
-const API_HOST = '128.199.158.226:13309'
-const path = ''
-const method = 'GET'
-
-// window.fetch(`http://${API_HOST}/${path}`, {
-//   headers,
-//   method,
-// }).then(res => {})
-
-// e.target.checked
-// dangerouslySetInnerHTM
