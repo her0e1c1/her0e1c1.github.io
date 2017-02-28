930654796092b9ebca6c774075ed8f93caf65e0b
her0e1c1 <her0e1c1@gmail.com>
2017-02-28 21:44:41 +0800

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { Alert, Button } from 'react-bootstrap';
+import { Alert, Button } from 'react-bootstrap'
+import {Socket} from './../phoenix.js'
 
 import Input from './Input/index.jsx'
 import Message from './Message.jsx'
 import Topic from './Topic.jsx'
 import Header from './Header.jsx'
 
-import {Socket} from './../phoenix.js'
 const t = require('assets/lang/ja.json')
 
 const WEBSOCKET_URL = __WEBSOCKET_URL__
 
 import $script from 'scriptjs'
-
-$script('//cdn.rawgit.com/her0e1c1/schemejs/master/main.js', 'schemejs')
 
 const code = `\
 (begin
@@ -26,23 +24,25 @@ class Main extends React.Component {
   constructor(props) {
     super(props)
 
-    const socket = new Socket(WEBSOCKET_URL + "1")
+    const socket = new Socket(WEBSOCKET_URL)
     console.log(socket)
     socket.connect()
     socket.onError(() => this.setState({showConnectionError: true}))
     socket.onClose(() => console.log("the connection dropped"))
 
-    // import `parse`
-    $script.ready('schemejs', () => {
-      return 1
+    $script('//cdn.rawgit.com/her0e1c1/schemejs/master/main.js', () => {
+      // import `parse`
+      this.setState({showSchemajs: true})
     })
-    this.state = {socket, code, showConnectionError: false}
+    this.state = {socket, code, showConnectionError: false, showSchemajs: false}
   }
   render() {
-    const {socket, code, showConnectionError} = this.state
+    const {socket, code, showConnectionError, showSchemajs} = this.state
     return (
     <div>
       <Header />
+
+      <h1>{t.hello}</h1>
 
       {showConnectionError && 
        <Alert bsStyle="danger" onDismiss={() => false}>
@@ -50,9 +50,12 @@ class Main extends React.Component {
        </Alert>
       }
 
-      {t.hello}<br />
-      <textarea rows="5" cols="40" value={code} onChange={e => this.setState({code: e.target.value}) }/>
-      <br/><Button onClick={() => parse(code) }>Run</Button>
+      {showSchemajs &&
+       <div>
+         <textarea rows="5" cols="40" value={code} onChange={e => this.setState({code: e.target.value}) }/>
+         <br/><Button onClick={() => parse(code) }>Run</Button>
+        </div>
+      }
     </div>
   )}
 }
