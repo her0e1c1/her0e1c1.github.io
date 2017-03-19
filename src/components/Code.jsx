b0840e113f9f24ba9bafeb039caa864813b993a3
her0e1c1 <her0e1c1@gmail.com>
2017-03-19 18:06:01 +0800

    Add Lang component and refactor code

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -2,6 +2,7 @@ import path from 'path'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Alert, Button } from 'react-bootstrap'
+import { NavDropdown, MenuItem } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import Highlight from 'react-highlight'
 import Swipeable from 'react-swipeable'
@@ -14,32 +15,56 @@ const fetchCode = (path) => new Promise((resolve, reject) => {
     .then(r => resolve(r.text()))
 })
 
-const List = ({parent, names}) => (
+const List = ({parent}) => {
+  const codes = parent.state.codes
+  return (
   <ul style={{ fontSize: "1.5em"}}>
-    {names.map((n, i) =>
-      <li key={i} onClick={() => parent.setState({index: i, showList: false})}>{n}</li>
+    {codes.filter(code => !parent.state.lang || code.name.endsWith(parent.state.lang)).map(code =>
+      <li key={code.i} onClick={() => parent.setState({index: code.i, showList: false})}>{code.name}</li>
      )}
- </ul>)
+  </ul>
+  )}
+
+const getExt = name => path.extname(name).substr(1)  // skip the first "."
+
+const Lang = ({parent}) => {
+  const l = new Set(parent.state.codes.map(code => getExt(code.name)))
+  return (
+    <NavDropdown title={parent.state.lang || "language"} id="language">
+    {[...l].map(lang =>
+      <MenuItem key={lang} active={lang == parent.state.lang}
+       onClick={() => parent.setState({lang})}>{lang}
+      </MenuItem>
+    )}
+  </NavDropdown>
+  )}
 
 class Code extends React.Component {
   constructor(props) {
     super(props)
     const index = parseInt(window.localStorage.getItem("index")) || 0
-    this.state = {index, codes: [], disableScroll: false, showList: false}
+    const lang = window.localStorage.getItem("lang")
+    this.state = {
+      codes: [],
+      disableScroll: false,
+      showList: false,
+      lang,
+      index
+    }
     this.next = this.next.bind(this)
     this.prev = this.prev.bind(this)
   }
 
   componentDidMount() {
     let codes = []
-    __CODES__.forEach(name => {
+    __CODES__.forEach((name, i) => {
       const code = window.localStorage.getItem(name)
       if (code != undefined) {
-        codes = [...codes, {code, name}]
+        codes = [...codes, {code, name, i}]
       } else {
         fetchCode(name).then(code => {
           window.localStorage.setItem(name, code)
-          codes = [...codes, {code, name}]
+          codes = [...codes, {code, name, i}]
           this.setState({codes})
         })
       }
@@ -51,6 +76,7 @@ class Code extends React.Component {
     if (typeof MathJax !== "undefined")
       MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
     window.localStorage.setItem("index", this.state.index)
+    window.localStorage.setItem("lang", this.state.lang)
   }
 
   next() {
@@ -83,25 +109,27 @@ class Code extends React.Component {
       code = codes[index].code
       name = codes[index].name
     }
-    const ext = path.extname(name).substr(1)  // skip the first "."
+    const ext = getExt(name)
     return (
       <div>
-        {showList &&<List parent={this} names={__CODES__}/>}
+        {showList &&<List parent={this} />}
         <div style={{position: "fixed", backgroundColor: "black", width: "100%", fontSize: "1.2em"}}>
-          <button type="button" onClick={this.prev}>Prev</button>
-          <button type="button" onClick={this.next}>Next</button>
-          {disableScroll ? "ON" : "OFF"}
-          {' '}{index}/{codes.length-1}
-          {/*' '}{name*/}
-          {' '}<Button onClick={() => this.setState({showList: true})}>CODES</Button>
-          {' '}<Button onClick={() => fetchCode(name).then(code => {
+          <ul style={{ border: "1px solid blue" }} className="list-inline">
+            <li><Button bsSize="xsmall" onClick={this.prev}>Prev</Button></li>
+            <li><Button bsSize="xsmall" onClick={this.next}>Next</Button></li>
+            <li>{disableScroll ? "ON" : "OFF"}</li>
+            <li>{index}/{codes.length-1}</li>
+            <li><Button bsSize="xsmall" onClick={() => this.setState({showList: true})}>CODES</Button></li>
+            <li><Button bsSize="xsmall" onClick={() => fetchCode(name).then(code => {
             let codes = this.state.codes
             window.localStorage.setItem(name, code)
-            codes[index] = {code, name}
+            codes[index] = {code, name, i: index}
             this.setState({codes})})
-          } >UPDATE</Button>
+            } >UPDATE</Button></li>
+            <Lang parent={this} />
+          </ul>
         </div>
-        <div style={{paddingTop: "15px"}}>
+        <div style={{paddingTop: "20px"}}>
           <Swipeable
             onSwipedRight={this.prev}
             onSwipedLeft={this.next}
