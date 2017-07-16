f88f963c38a9db1ad1d695800aa1d1d5fe013d3b
her0e1c1 <her0e1c1@gmail.com>
2017-07-16 22:02:14 +0800

    Add Chart interface

--- src/components/Chart/Interface.ts
+++ src/components/Chart/Interface.ts
@@ -5,14 +5,17 @@ const SignalTypes: SignalType[] = ["BUY", "SELL"];
 type SignalKey = "macd_signal" | "rsi" | "stochastic" | "bollinger_band" | "rolling_mean";
 const SignalKeys: SignalKey[] = ["macd_signal", "rsi", "stochastic", "bollinger_band", "rolling_mean"];
 
+type price = number;
+type timepstamp = number; // millisecond
+
 interface Price {
   open?: number;
   high?: number;
   low?: number;
   close?: number;
   volume: number;
-  date: number; // millisecond
-  code?: Code;
+  date: timepstamp;
+  code: Code;
 }
 
 interface SignalObj {
@@ -31,4 +34,47 @@ interface Code {
   signal?: Signal;
 }
 
-export { SignalType, Price, Signal, Code, SignalKeys, SignalTypes, SignalKey };
+type Line = [[timepstamp, price]];
+type OHLC = Price[];
+
+interface Chart {
+  code: string;
+  ohlc?: OHLC;
+  rsi?: {
+    line: Line;
+  }
+  stochastic?: {
+    k: Line;
+    d: Line;
+    sd: Line;
+  }
+  macd?: {
+    line: Line;
+    signal: Line;
+  }
+  rolling_mean?: {
+    line25: Line;
+    line50: Line;
+    line200: Line;
+  }
+  bollinger_band?: {
+    sigma1: Line;
+    sigma2: Line;
+    sigma3: Line;
+    sigma1m: Line; // minus
+    sigma2m: Line;
+    sigma3m: Line;
+  } 
+}
+
+// highchart
+interface Series {
+  type: "line";
+  data: Line;
+  lineWidth: number;
+  yAxis: number;
+  name: string;
+  color: string;
+}
+
+export { SignalType, Price, Signal, Code, SignalKeys, SignalTypes, SignalKey, Chart };
